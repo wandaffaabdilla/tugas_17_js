bedb56995701a60d990f4e6303b40e281da12c5c
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-02-17 23:04:31 +0800

    Tugas 17 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas17.js
@@ -0,0 +1,25 @@
+function tugas17() {
+  var kata_a = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var kata_b =
+    "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+  let kode =
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+  console.log(kata_a.toLowerCase());
+  console.log(kata_a.toUpperCase());
+  console.log(kata_b.toLowerCase());
+  console.log(kata_b.toUpperCase());
+  console.log(
+    kode.charCodeAt(0),
+    "-",
+    kode.charCodeAt(1),
+    "-",
+    kode.charCodeAt(2),
+    "-",
+    kode.charCodeAt(3),
+    "-",
+    kode.charCodeAt(4),
+    "-",
+    kode.charCodeAt(5)
+  );
+}
+tugas17();
